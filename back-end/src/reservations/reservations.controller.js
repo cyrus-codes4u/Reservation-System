562afd9e409c0344643299b3cc825991ffb0e0bf
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-09 15:22:55 -0400

    reservations.controller.js implemented functions to respond to requests with all or a specific reservation

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -1,12 +1,34 @@
+const service = require("./reservations.service")
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
+
 /**
- * List handler for reservation resources
+ * GET handler for all reservation entries
  */
 async function list(req, res) {
-  res.json({
-    data: [],
+  const {date} = req.query
+  const data = await service.list()
+  res.status(200).send({
+    data: data,
   });
+}
+/**
+ * GET handler for specific reservation
+ */
+async function read(req, res, next) {
+  const {reservation_id} = req.params
+  const data = await service.read(reservation_id)
+  if(data){
+    res.status(200).send({ data: data});
+  }
+  next(
+    {
+      error: 404,
+      message: `Reservation with ${reservation_id} not found.`
+    }
+  )
 }
 
 module.exports = {
-  list,
+  list: asyncErrorBoundary(list),
+  read: asyncErrorBoundary(read)
 };
