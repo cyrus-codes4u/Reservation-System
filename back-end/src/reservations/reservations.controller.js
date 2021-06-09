8d8261272a2f0089d1dc76095175d197dd55a47b
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-09 17:22:36 -0400

    reservations.controller.js added validation middleware checking all reuired properties are present and not empty

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -5,11 +5,9 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
  * GET handler for all reservation entries
  */
 async function list(req, res) {
-  const {date} = req.query
-  const data = await service.list()
-  res.status(200).send({
-    data: data,
-  });
+  const {date = null} = req.query
+  const data = await service.list(date)
+  res.status(200).json({ data: data });
 }
 /**
  * GET handler for specific reservation
@@ -22,13 +20,57 @@ async function read(req, res, next) {
   }
   next(
     {
-      error: 404,
+      status: 404,
       message: `Reservation with ${reservation_id} not found.`
     }
   )
 }
 
+function hasData(req, res, next){
+  if(req.body.data){
+    res.locals.reservation = req.body.data
+    return next()
+  }
+  next(
+    {
+      status: 400,
+      message: "Request body does not have any data"
+    }
+  )
+}
+
+function hasCorrectProperties(req,res,next){
+  const validKeys = ["first_name", "last_name", "mobile_number", "reservation_date", "reservation_time","people"]
+  for(let i = 0; i< validKeys.length; i++){
+    const key = validKeys[i]
+    if(!res.locals.reservation[key]){
+      next(
+        {
+          status: 400,
+          message: `Request does not contain ${key} property.`
+        }
+      )
+    }
+    if(!res.locals.reservation[key].length){
+      next({
+        status:400,
+        message: `Request ${key} field must not be empty.`
+      })
+    }
+
+  }
+  next()
+}
+
+
+async function create(req,res,next){
+  const newReservation = res.locals.reservation
+  const storedReservation = await service.create(newReservation)
+  res.status(201).json({ data: storedReservation})
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
-  read: asyncErrorBoundary(read)
+  read: asyncErrorBoundary(read),
+  create: [asyncErrorBoundary(hasData), asyncErrorBoundary(hasCorrectProperties), asyncErrorBoundary(create)],
 };
