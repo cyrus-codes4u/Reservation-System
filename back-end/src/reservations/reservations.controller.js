0a428d5130867d1da21807bda63cf56b47e659fc
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-12 14:08:10 -0400

    Validation middleware in reservations.controller.js check reservations are in the future and not on tuesday

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -38,7 +38,6 @@ function hasData(req, res, next){
     }
   )
 }
-
 function hasRequiredProperties(req,res,next){
   const validKeys = ["first_name", "last_name", "mobile_number", "reservation_date", "reservation_time","people"]
   for(let i = 0; i< validKeys.length; i++){
@@ -55,10 +54,29 @@ function hasRequiredProperties(req,res,next){
   next()
 }
 function datePropIsADate(req,res,next){
-  
+  const dateRegex = new RegExp('^[12][0-9]{3}-0[1-9]|1[0-2]-0[1-9]|[12][0-9]|3[01]$')
+  if(dateRegex.test(res.locals.reservation.reservation_date)){
+    return next()
+  }
+  next(
+    {
+      status:400,
+      message: `reservation_date: ${res.locals.reservation.reservation_date} is not a date`
+    }
+  )
 }
 function timePropIsATime(req,res,next){
+  const timeRegex = new RegExp('^([2][0-3]|[01][0-9]):[0-5][0-9]$')
   
+  if(timeRegex.test(res.locals.reservation.reservation_time)){
+    return next()
+  }
+  next(
+    {
+      status:400,
+      message: `reservation_time: ${res.locals.reservation.reservation_time} is not a time`
+    }
+  )
 }
 function hasNoEmptyProperties (req,res,next){
   Object.entries(res.locals.reservation).forEach(([key, value]) => {
@@ -71,7 +89,6 @@ function hasNoEmptyProperties (req,res,next){
   })
   next()
 }
-
 function peopleIsNonZeroInteger(req,res,next){
   if(Number.isInteger(res.locals.reservation.people) && res.locals.reservation.people !== 0){
     return next()
@@ -83,15 +100,42 @@ function peopleIsNonZeroInteger(req,res,next){
     }
   )
 }
+function dateIsInFuture(req,res,next){
+  const today = new Date()
+  const [year, month, day] = res.locals.reservation.reservation_date.split("-")
+  res.locals.reservationDateObject = new Date(year, month-1, day)
+  if(res.locals.reservationDateObject - today > 0){
+    return next()
+  }
+  next(
+    {
+      status:400,
+      message: "Reservation date must be placed for a future time."
+    }
+  )
+}
+
+function dateIsNotTuesday(req,res,next){
+  if( res.locals.reservationDateObject.getDay() !== 2){
+    return next()
+  }
+  next(
+    {
+      status: 400,
+      message: "The restaurant is closed on Tuesdays."
+    }
+  )
+}
 
 async function create(req,res,next){
-  const newReservation = res.locals.reservation
-  const storedReservation = await service.create(newReservation)
+  const storedReservation = await service.create(res.locals.reservation)
   res.status(201).json({ data: storedReservation[0] })
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
   read: asyncErrorBoundary(read),
-  create: [asyncErrorBoundary(hasData), asyncErrorBoundary(hasRequiredProperties), asyncErrorBoundary(hasNoEmptyProperties), asyncErrorBoundary(peopleIsNonZeroInteger), asyncErrorBoundary(create)],
+  create: [asyncErrorBoundary(hasData), asyncErrorBoundary(hasRequiredProperties), asyncErrorBoundary(hasNoEmptyProperties),
+    asyncErrorBoundary(datePropIsADate), asyncErrorBoundary(timePropIsATime), asyncErrorBoundary(peopleIsNonZeroInteger), 
+    asyncErrorBoundary(dateIsInFuture), asyncErrorBoundary(dateIsNotTuesday), asyncErrorBoundary(create)],
 };
