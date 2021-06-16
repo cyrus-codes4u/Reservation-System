28af557e9969ef472a66b1c7f5cfdf2460c8b304
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-16 13:08:14 -0400

    Fix typo

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -4,8 +4,8 @@
  * @type {Router}
  */
 
-const router = require("express").Router();
-const controller = require("./tables.controller");
+const router = require("express").Router()
+const controller = require("./tables.controller")
  
 const methodNotAllowed = require("../errors/methodNotAllowed.js")
 
@@ -13,7 +13,7 @@ router.route("/:table_id/seat")
     .put(controller.update)
     .all(methodNotAllowed)
 
-router.route("/table_id")
+router.route("/:table_id")
     .get(controller.read)
     .all(methodNotAllowed)
 
