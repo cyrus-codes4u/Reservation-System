52e76946aab779bcbea7591f0456b02f3b85e530
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-16 13:07:46 -0400

    Added /:table_id route to tables resource router backend/src/tables/tables.router.js

--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -4,18 +4,22 @@
  * @type {Router}
  */
 
- const router = require("express").Router();
- const controller = require("./tables.controller");
+const router = require("express").Router();
+const controller = require("./tables.controller");
  
- const methodNotAllowed = require("../errors/methodNotAllowed.js")
+const methodNotAllowed = require("../errors/methodNotAllowed.js")
 
- router.route("/:table_id/seat")
+router.route("/:table_id/seat")
     .put(controller.update)
     .all(methodNotAllowed)
 
- router.route("/")
-    .get(controller.list) 
-    .post(controller.create) // creates a table in db
+router.route("/table_id")
+    .get(controller.read)
     .all(methodNotAllowed)
 
- module.exports = router;
+router.route("/")
+    .get(controller.list) 
+    .post(controller.create)
+    .all(methodNotAllowed)
+
+module.exports = router;
