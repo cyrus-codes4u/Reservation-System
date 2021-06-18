4c1ab36c111970d4c0fcc26b6911149e6b5cf537
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-17 20:00:51 -0400

    Changed update function to a knex transaction: as reservation status is updated so too is reservation_id property of mathcing table entry; added remove function: as a reservation finishes, the reservation status is changed to 'finished' and table reservation_id changed to null

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -20,15 +20,40 @@ function create(newTable){
 }
 
 function update(id, updates){
-    return knex("tables")
-        .where({table_id: id})
-        .update(updates)
-        .returning(["table_name", "table_id", "capacity", "reservation_id"])
+    return knex.transaction ( (trx) => {
+        return knex("reservations")
+            .transacting(trx)
+            .where({reservation_id: updates.reservation_id})
+            .update({status: "seated" })
+            .then(() => {
+                return knex("tables")
+                    .where({table_id: id})
+                    .update(updates)
+                    .returning(["table_name", "table_id", "capacity", "reservation_id"])                    
+            })
+    })
+}
+
+function remove(id){
+    return knex.transaction( async (trx) => {
+        const table = await knex("tables").where({table_id: id}).first()
+        return knex("reservations")            
+            .transacting(trx)
+            .where({reservation_id: table.reservation_id })
+            .update({status: "finished" })
+            .then(() => {
+                return knex("tables")
+                    .where({table_id: id})
+                    .update({reservation_id: null})
+                    .returning(["table_name", "table_id", "capacity", "reservation_id"])
+            })
+    })
 }
 
 module.exports ={
     list,
     read,
     create,
-    update
+    update,
+    remove,
 }
