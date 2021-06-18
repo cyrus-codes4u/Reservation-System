a85f624a99314960bcec2b9f2c648c6f414ad38c
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-17 20:23:02 -0400

    Added function to update database reservation entry to change status; also a filter to only list unfinished reservations in backend/src/reservations/reservations.service

--- back-end/src/reservations/reservations.service.js
+++ back-end/src/reservations/reservations.service.js
@@ -1,16 +1,17 @@
 const knex = require("../db/connection")
 
 function list(date){
-        return knex
-            .from("reservations")
-            .select("*")
-            .where({reservation_date : date})
-            .orderBy("reservation_time")
+    return knex
+        .from("reservations")
+        .select("first_name", "last_name", "mobile_number", "reservation_date", "reservation_time", "people", "reservation_id", "status")
+        .whereNot({status: "finished"})
+        .andWhere({reservation_date : date})
+        .orderBy("reservation_time")
 }
 
 function read(id){
     return knex("reservations")
-        .select("first_name", "last_name", "mobile_number", "reservation_date", "reservation_time", "people", "reservation_id")
+        .select("first_name", "last_name", "mobile_number", "reservation_date", "reservation_time", "people", "reservation_id", "status")
         .where({reservation_id: id})
         .first()
 }
@@ -18,11 +19,20 @@ function read(id){
 function create(reservation){
     return knex("reservations")
         .insert(reservation)
-        .returning(["first_name", "last_name", "mobile_number", "reservation_date", "reservation_time", "people"])
+        .returning(["first_name", "last_name", "mobile_number", "reservation_date", "reservation_time", "people", "reservation_id", "status"])
+}
+
+function update(id, updates){
+    return knex("reservations")
+        .where({reservation_id: id})
+        .update(updates)
+        .returning(["first_name", "last_name", "mobile_number", "reservation_date", "reservation_time", "people", "reservation_id", "status"])
+        .then((results) => results[0] )
 }
 
 module.exports ={
     list,
     read,
     create,
+    update
 }
