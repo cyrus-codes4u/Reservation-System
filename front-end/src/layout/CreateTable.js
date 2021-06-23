f75e2be30da8e246226738c1ea570a88342d5daa
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-23 10:39:40 -0400

    Moved create table form component to layout directory in front-end

---
+++ front-end/src/layout/CreateTable.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom"
+import { createTable } from "../utils/api"
+
+function CreateTable(){
+    const initialFormState = {
+        table_name: "",
+        capacity: 1,
+    }
+    const [formState,setFormState] = useState({...initialFormState})
+    const history = useHistory()
+
+
+    const updateForm = ({target}) => {
+        setFormState({...formState, [target.name]: target.value})
+    }
+
+    const submitHandle = (event) => {
+        event.preventDefault()
+        createTable(formState)
+            .then(() => history.push("/dashboard"))
+    }
+    const cancelHandle = () => {
+        history.goBack()
+    }
+    
+    return (
+        <React.Fragment>
+            <form onSubmit={submitHandle}>
+                <label htmlFor="table_name">Table Name</label>
+                <input 
+                    name="table_name"
+                    type="text"
+                    id ="table_name"
+                    value = {formState.table_name}
+                    onChange={updateForm}
+                    required
+                    autoFocus
+                    autoCapitalize="true"
+                />
+                <label htmlFor="capacity">Capacity</label>
+                <input 
+                    name="capacity"
+                    type="number"
+                    id ="capacity"
+                    value = {formState.capacity}
+                    onChange={updateForm}
+                    min="1"
+                    required
+                />
+                <button type="submit">Submit</button>
+                <button type="button" onClick={cancelHandle}>Cancel</button>
+            </form>
+        </React.Fragment>
+    )
+}
+
+export default CreateTable
