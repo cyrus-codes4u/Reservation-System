1f43f1c200d95065e52b907c17363787e18820cc
Cyrus Glanzer <cyrusglanzer@gmail.com>
2021-06-26 15:33:27 -0400

    Added cancel button beside reservation list items; filtered out cancelled and finished reservations

--- front-end/src/dashboard/ReservationList.js
+++ front-end/src/dashboard/ReservationList.js
@@ -1,21 +1,35 @@
 import React from "react";
-import { formatAsTime } from "../utils/date-time";
+import { formatAsTime, formatAsDate } from "../utils/date-time";
+import { updateReservationStatus } from "../utils/api";
 
 
 function ReservationList({reservations}){
     if(!reservations) {return null}
     const listOfReservations = reservations.map((reservation, index) => {
+      const cancelHandle = (id) => {
+        const response = window.confirm("Are you sure you want to cancel this reservation?\nThis action cannot be undone.")
+        if(response){
+          updateReservationStatus(id, "cancelled")
+        }
+      }
+
+      if (reservation.status === "cancelled" || reservation.status === "finished") { return null}
       return (
         <li key= {`id_${index}`} > 
           <p>Name: {reservation.last_name}, {reservation.first_name} </p>
-          <p>Date: {reservation.reservation_date}</p>
+          <p>Date: {formatAsDate(reservation.reservation_date)}</p>
           <p>Time: {formatAsTime(reservation.reservation_time)} </p>
           <p>Party of {reservation.people}</p>
           <p data-reservation-id-status={reservation.reservation_id}>Status: {reservation.status}</p>
+          
           {reservation.status==="booked" ? 
-            <a className="btn btn-primary" href={`/reservations/${reservation.reservation_id}/seat`}>Seat</a>
+            <React.Fragment>
+              <a className="btn btn-primary" href={`/reservations/${reservation.reservation_id}/seat`}>Seat</a>
+              <a className="btn btn-secondary" href={`/reservations/${reservation.reservation_id}/edit`}>Edit</a>
+            </React.Fragment>
             : null }
-          <a className="btn btn-secondary" href={`/reservations/${reservation.reservation_id}/edit`}>Edit</a>
+  
+          <button className="btn btn-danger" onClick={() => cancelHandle(reservation.reservation_id)}>Cancel</button>
         </li>
       )
     })
